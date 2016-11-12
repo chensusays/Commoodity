d678d1ae36e66f9374c773113fe41db1f429273a
chensusays <chensusays@gmail.com>
2016-11-12 18:43:27 -0500

    some code

--- comoodity/comoodity.ino
+++ comoodity/comoodity.ino
@@ -1,26 +1,35 @@
 #define MAX_HEALTH = 8
 
 uint_32 health;
-uint_32 initTime; 
-bool buttonPress;
-int numPresses;
-
+long time = 0; 
+bool buttonPress = false;
+int numPresses = 0;
+int inPin = 2;         // the number of the input pin
+int outPin = 13;       // the number of the output pin
+ 
 void setup() {
   // put your setup code here, to run once:
-  initTime = 0;
-  numPresses = 0;
-  buttonPress = false;
-  
+  pinMode(inPin, INPUT);
+  digitalWrite(inPin, HIGH);   // turn on the built in pull-up resistor
+  pinMode(outPin, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   tilted = false
+  buttonPress = digitalRead(2);
   if(tilted) {
     //start timer for health regen
   } else if(buttonPress) {
+    health++;
+    numPresses++;
+    if (numPresses >= 5) {
+      numPresses = 0;
+      //reset
+    }
     //increment health by one
     //increment numPresses by one
+
   } else {
     //wait for countdown to reach x time
     
