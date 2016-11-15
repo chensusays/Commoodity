850156df88f80d75759478da76775517137e735c
FiscalTiger <apouleson@gmail.com>
2016-11-14 20:27:31 -0500

    Tilt sensor, button, and timer implemented

--- comoodity/comoodity.ino
+++ comoodity/comoodity.ino
@@ -1,19 +1,23 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_NeoMatrix.h>
 #include <Adafruit_NeoPixel.h>
+#include <TimeLib.h>
 
-#define MAX_HEALTH = 8
+#define MAX_HEALTH  8
+#define TIME_THRESHOLD 10000
 #define BRIGHTNESS 35
 #define RED matrix.Color(255, 0, 0)
 #define WHITE matrix.Color(255, 255, 255)
 #define OFF matrix.Color(0, 0, 0)
 
-uint_32 health;
-long time = 0; 
+int health;
+time_t epoch;
 bool buttonPress = false;
+bool tilted = false;
 int numPresses = 0;
-int inPin = 2;         // the number of the input pin
-int outPin = 13;       // the number of the output pin
+int tiltInPin = 10;         // the number of the tilt sensor's input pin
+int buttonInPin = 9;        // the number of the button pin
+int outPin = 12;       // the number of the output pin
 
 Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(8, 8, outPin,
       NEO_MATRIX_TOP     + NEO_MATRIX_RIGHT +
@@ -47,31 +51,48 @@ void display_health(int health) {
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(inPin, INPUT);
-  digitalWrite(inPin, HIGH);   // turn on the built in pull-up resistor
+  pinMode(tiltInPin, INPUT);
+  pinMode(buttonInPin, INPUT);
+  digitalWrite(tiltInPin, HIGH);   // turn on the built in pull-up resistor
+  digitalWrite(buttonInPin, HIGH);
   pinMode(outPin, OUTPUT);
+  epoch = now();
+  
   matrix.begin();
   matrix.setBrightness(BRIGHTNESS);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  tilted = false
-  buttonPress = digitalRead(2);
+  time_t cur_time = now();
+  tilted = digitalRead(tiltInPin);
+  buttonPress = digitalRead(buttonInPin);
   if(tilted) {
     //start timer for health regen
-  } else if(buttonPress) {
-    health++;
-    numPresses++;
-    if (numPresses >= 5) {
-      numPresses = 0;
-      //reset
+    time_t start_laying_down = now();
+    time_t end_laying_down = now();
+    while(digitalRead(tiltInPin)) {
+      end_laying_down = now();
     }
-    //increment health by one
-    //increment numPresses by one
 
+    time_t dur = end_laying_down - start_laying_down;
+
+    int hours = dur/TIME_THRESHOLD;
+    health = hours + health > MAX_HEALTH ? MAX_HEALTH : hours + health;
+  } else if(buttonPress) {
+    if(numPresses <= 5) {
+        health = health + 1 >= MAX_HEALTH ? MAX_HEALTH : health + 1;     //increment health by one
+        numPresses++; //increment numPresses by one
+    }
   } else {
     //wait for countdown to reach x time
-    
+    time_t dur = cur_time - epoch;
+
+    if(dur >= TIME_THRESHOLD) {
+      health = health - 1 <= 0 ? 0 : health - 1;
+      epoch = now();
+    }
   }
+
+  display_health(health);
 }
